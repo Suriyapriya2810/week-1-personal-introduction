5594fd685f69eb8e3050115e5ac8489511ecb733
Suriyapriya2810 <suriyakandhavel2810@gmail.com>
2026-05-12 10:14:46 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,15 @@
+ # Week 1: Program about my personal introduction
+#Heading
+print("Bio-Data")
+    #Input,storing data by using variables
+Name = input("Enter your name: ")
+Age = input("Enter your age: ")
+City = input("Enter your city: ")
+Hobby = input("What is your favorite hobby? ")
+Qualification=input("What is your Qualification:")
+    # welcome message
+print(f"\n Welcome {Name} from {City},Nice to meet you!")
+print(f"It's awesome that you are {Age} years old and enjoy {Hobby}.")
+print(f"Oh Great,you completed {Qualification}.")
+
+
